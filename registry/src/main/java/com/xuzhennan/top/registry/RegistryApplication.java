67f67edefaff3bcb4e72989fd24455ad2d84f9c9
Andrew <522988922@qq.com>
2020-03-09 13:35:38 +0800

    init

--- registry/src/main/java/com/xuzhennan/top/registry/RegistryApplication.java
+++ registry/src/main/java/com/xuzhennan/top/registry/RegistryApplication.java
@@ -2,8 +2,10 @@ package com.xuzhennan.top.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableConfigServer
 @EnableEurekaServer
 @SpringBootApplication
 public class RegistryApplication {
@@ -11,5 +13,6 @@ public class RegistryApplication {
     public static void main(String[] args) {
         SpringApplication.run(RegistryApplication.class, args);
     }
+
 
 }
