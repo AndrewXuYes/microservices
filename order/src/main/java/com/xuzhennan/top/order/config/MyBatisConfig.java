c6ec0a44fe66640f091de4d420d95ff29b23c125
Andrew <522988922@qq.com>
2020-03-11 17:31:18 +0800

    Init Order

--- order/src/main/java/com/xuzhennan/top/order/config/MyBatisConfig.java
+++ order/src/main/java/com/xuzhennan/top/order/config/MyBatisConfig.java
@@ -1,11 +1,22 @@
 package com.xuzhennan.top.order.config;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableTransactionManagement //开启事务
-@MapperScan({"com.xuzhennan.top.order.mapper", "com.xuzhennan.top.order.dao"})
+@MapperScan({"com.*"})
 public class MyBatisConfig {
+
+//    @Bean
+//    @ConfigurationProperties(prefix = "spring.datasource")
+//    public DataSource druid() {
+//        return new DruidDataSource();
+//    }
 }
