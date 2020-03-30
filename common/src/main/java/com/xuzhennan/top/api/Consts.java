b37ff8bd5cd8931e60a5905385509571cce0fb39
Andrew <522988922@qq.com>
2020-03-30 22:46:17 +0800

    springsession

---
+++ common/src/main/java/com/xuzhennan/top/api/Consts.java
@@ -0,0 +1,12 @@
+package com.xuzhennan.top.api;
+
+/**
+ * @author andrew
+ * @date 2020/3/30 22:27
+ */
+public interface Consts {
+    /**
+     * session保存的key
+     */
+    String SESSION_KEY = "key:session:token";
+}
