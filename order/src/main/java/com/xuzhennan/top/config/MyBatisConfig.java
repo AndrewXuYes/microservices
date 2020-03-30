83f290a2b19013857274561ea36e78d8ef546954
Andrew <522988922@qq.com>
2020-03-30 23:05:27 +0800

    springsession

--- order/src/main/java/com/xuzhennan/top/config/MyBatisConfig.java
+++ order/src/main/java/com/xuzhennan/top/config/MyBatisConfig.java
@@ -1,12 +1,10 @@
 package com.xuzhennan.top.config;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement //开启事务
-@MapperScan({"com.xuzhennan.top.mapper"})
 public class MyBatisConfig {
 
 }
